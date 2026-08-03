24f237c024e28a9c8b4dfe88de95da5d4c1f8855
Yu Ying Li Wen <usbthekey@gmail.com>
2026-08-03 16:11:15 -0400

    Add more images

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,6 +1,43 @@
-import yurock from "../assets/cornell_box.jpg";
+import brickBreaker from "../assets/brick_breaker.gif";
+import cornellBox from "../assets/cornell_box.jpg";
+import danbar from "../assets/Danbar.gif";
+import logicalCircuit from "../assets/Logical_circuit.gif";
+import playground from "../assets/playground.gif";
+import terrain from "../assets/terrain.jpg";
+import yurock from "../assets/yurock.gif";
+
 
 export default [
+    {
+        title: "Brick Breaker",
+        image: brickBreaker,
+        description: "A classic brick breaker game project."
+    },
+    {
+        title: "Cornell Box",
+        image: cornellBox,
+        description: "A Cornell Box rendering project."
+    },
+    {
+        title: "Danbar",
+        image: danbar,
+        description: "A project showcasing the Danbar system."
+    },
+    {
+        title: "Logical Circuit",
+        image: logicalCircuit,
+        description: "A digital logic circuit simulation project."
+    },
+    {
+        title: "Playground",
+        image: playground,
+        description: "A playground environment project."
+    },
+    {
+        title: "Terrain",
+        image: terrain,
+        description: "A terrain generation and environment project."
+    },
     {
         title: "Yurock",
         image: yurock,
